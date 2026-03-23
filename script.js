6bdc474a81270d1a10da2d33033a4b306c994447
Atharva Lonikar <126399397+atharvalonikar95@users.noreply.github.com>
2026-03-23 16:51:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
-function highlight() {
+const words= document.querySelectorAll('strong')
+function highlight(word) {
     //Write your code here
-
+	words.forEach((word)=>{
+		word.style.color="rgb(0, 128, 0)"
+	})
 
 }
 
 
 function return_normal() {
     //Write your code here
-
+	words.forEach((word)=>{
+		word.style.color="rgb(0, 0, 0)"
+	})
     
 }
